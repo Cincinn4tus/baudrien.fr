89aa5fcc76c4d3d17dd097c81ae768ada9af6726
Cincinn4tus <>
2022-08-21 20:12:21 +0200

    test liens

--- login.php
+++ login.php
@@ -31,10 +31,10 @@
                         if($_SESSION['id'] == 1) {
 
 
-						header("location: admin.php");
+						header("location: ./admin.php");
 
                         } else {
-                            header("Location: index.php");
+                            header("Location: ./index.php");
                         }
 
 					}else{
