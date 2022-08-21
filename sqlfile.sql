8059bfdd940b145291fc0486f5ba36f308eb1e08
Cincinn4tus <>
2022-08-21 16:51:20 +0200

    fix

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,2 +1,8 @@
-CREATE TABLE `baudrien_user`
-  "id" int(11) NOT NULL;
+CREATE TABLE AVION(
+    immatriculation INTEGER PRIMARY KEY,
+    nom VARCHAR(10),
+    constructeur VARCHAR(30),
+    capacite INTEGER CHECK (capacite >0),
+    nombre_reacteurs INTEGER CHECK( nombre_reacteurs>0),
+    largeur INTEGER
+);
