becc18c55d40e2447f314f6b894ce847ed85b0b8
Cincinn4tus <110392090+Cincinn4tus@users.noreply.github.com>
2022-08-28 18:33:53 +0200

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -35,7 +35,7 @@
                         <div class="location-item d-flex justify-content-center">
                             <div id="location-div">
                                 <h3 class="location-title"> <?php echo $location['location_title']; ?></h3>
-                                <a href="<?php echo "location/" . urlencode($_GET["name"]);?>">
+                                <a href="<?php echo "location/" . urlencode($location["location_title"])  ;?>">
                                     <button class="btn btn-info">Ouvrir</button>
                                 </a>
                             </div>
